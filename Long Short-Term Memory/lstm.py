aeecd888f9ed58109f6c13e1e87dca38d11b894e
costineesti <chiticcostin@yahoo.com>
2024-11-05 23:08:49 +0200

    Need to finish LSTM as quick as possible; I have money to invest haha :p Also reorganized the code and changed the approach to only one weight matrix while concatenating the current input and the hidden layers:

--- Long Short-Term Memory/lstm.py
+++ Long Short-Term Memory/lstm.py
@@ -32,22 +32,18 @@ class LSTM:
         if xavier_init == 'normal':
             # Forget Gate Weights
             self.Wf = np.random.normal(0, normal_xavier_init, (hidden_dim, input_dim))
-            self.Uf = np.random.normal(0, normal_xavier_init, (hidden_dim, hidden_dim))
             self.bf = np.ones((1, hidden_dim)) # Initialize the forget gate bias to 1 to improve learning stability in the beginning.
             
             # Input Gate Weights
             self.Wi = np.random.normal(0, normal_xavier_init, (hidden_dim, input_dim))
-            self.Ui = np.random.normal(0, normal_xavier_init, (hidden_dim, hidden_dim))
             self.bi = np.zeros((1, hidden_dim))
             
             # Output Gate Weights
             self.Wo = np.random.normal(0, normal_xavier_init, (hidden_dim, input_dim))
-            self.Uo = np.random.normal(0, normal_xavier_init, (hidden_dim, hidden_dim))
             self.bo = np.zeros((1, hidden_dim))
             
             # Cell State Weights
             self.Wc = np.random.normal(0, normal_xavier_init, (hidden_dim, input_dim))
-            self.Uc = np.random.normal(0, normal_xavier_init, (hidden_dim, hidden_dim))
             self.bc = np.zeros((1, hidden_dim))
             
             # Final Gate Weights
@@ -55,19 +51,15 @@ class LSTM:
             self.bv = np.zeros((1, output_dim))
         elif xavier_init == 'uniform':
             self.Wf = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, input_dim))
-            self.Uf = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, hidden_dim))
             self.bf = np.ones((1, hidden_dim))
             
             self.Wi = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, input_dim))
-            self.Ui = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, hidden_dim))
             self.bi = np.zeros((1, hidden_dim))
             
             self.Wo = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, input_dim))
-            self.Uo = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, hidden_dim))
             self.bo = np.zeros((1, hidden_dim))
             
             self.Wc = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, input_dim))
-            self.Uc = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (hidden_dim, hidden_dim))
             self.bc = np.zeros((1, hidden_dim))
             
             self.Wv = np.random.uniform(-uniform_xavier_init, uniform_xavier_init, (output_dim, hidden_dim))
@@ -75,17 +67,21 @@ class LSTM:
 
         # Diffs (derivative of loss function w.r.t all parameters)
         self.dWf = np.zeros(self.Wf.shape)
-        self.dUf = np.zeros(self.Uf.shape)
         self.dbf = np.zeros(self.bf.shape)
         self.dWi = np.zeros(self.Wi.shape)
-        self.dUi = np.zeros(self.Ui.shape)
         self.dbi = np.zeros(self.bi.shape)
         self.dWo = np.zeros(self.Wo.shape)
-        self.dUo = np.zeros(self.Uo.shape)
         self.dbo = np.zeros(self.bo.shape)
         self.dWc = np.zeros(self.Wc.shape)
-        self.dUc = np.zeros(self.Uc.shape)
         self.dbc = np.zeros(self.bc.shape)
+        self.dWv = np.zeros(self.Wv.shape)
+        self.dbv = np.zeros(self.bv.shape)
+
+        # Hidden state and cell state
+        self.h = np.zeros((hidden_dim, 1))
+        self.cell_state = np.zeros((hidden_dim, 1))
+        # Declare outputs list
+        self.outputs = []
 
     def prepare_data(self, df):
         df['date_ordinal'] = df['date'].map(datetime.date.toordinal) # convert date to numerical input
@@ -123,28 +119,52 @@ class LSTM:
     
     
     """
-    Forget gate: how much of the previous cell state to keep. 1=keep, 0=forget
-    Try an use this: https://cnvrg.io/pytorch-lstm/
-    ht-1 = prior hidden state
-    Xt = Current input
-    Wf = Forget gate weights for the prior hidden state
-    bf = Forget gate bias
-    Uf = Forget gate weights for the current input
+    Forward Propagation: Go through the input data and calculate the output for each time step.
     """
-    def forget_gate(self, x, h, bf):
-        # Use the link above.
-        sigmoid_argument = bf + self.Wf @ x + self.Uf @ h
-        return self.sigmoid(sigmoid_argument)
-    
-    def input_gate(self, x, h, bi):
-        sigmoid_argument = bi + self.Wi @ x + self.Ui @ h
-        return self.sigmoid(sigmoid_argument)
     
     def forward_propagation(self):
         # https://medium.com/@CallMeTwitch/building-a-neural-network-zoo-from-scratch-the-long-short-term-memory-network-1cec5cf31b7
         # Just follow this implementation because it actually makes sense. At least I have a better understanding of what's going on.
-        pass
+        outputs = []
+
+        for i in range(len(self.X)):
+            concatenated_argument = np.concatenate((self.X[i], self.h), axis=1)
+            forget_gate = self.sigmoid(self.bf + np.dot(self.Wf, concatenated_argument))
+            input_gate = self.sigmoid(self.bi + np.dot(self.Wi, concatenated_argument))
+            candidate_gate = self.tanh(self.bc + np.dot(self.Wc, concatenated_argument))
+            output_gate = self.sigmoid(self.bo + np.dot(self.Wo, concatenated_argument))
+            
+            self.cell_state = forget_gate * self.cell_state + input_gate * candidate_gate
+            self.h = output_gate * self.tanh(self.cell_state)
+
+            output = self.Wv * self.h + self.bv
+            outputs.append(output)
+
+        return self.outputs
     
+    """
+    Backward Propagation: Calculate the gradients of the loss function w.r.t all parameters. Update weights and biases.
+    """
+    def back_propagation(self):
+        # reset gradients
+        self.dWf.fill(0)
+        self.dbf.fill(0)
+        self.dWi.fill(0)
+        self.dbi.fill(0)
+        self.dWo.fill(0)
+        self.dbo.fill(0)
+        self.dWc.fill(0)
+        self.dbc.fill(0)
+        self.dWv.fill(0)
+        self.dbv.fill(0)
+
+        d_h = np.zeros_like(self.h)
+        d_C = np.zeros_like(self.cell_state)
+
+        loss = 0
+
+        # Iterate backwards through times steps to calculate gradients
+        
     
 
 if __name__ == "__main__":
